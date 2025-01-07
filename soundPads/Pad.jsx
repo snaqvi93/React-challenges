c32eb52cd9c093a0935e164b8ea7ed7ceb58fb54
snaqvi93 <abbas_naq@icloud.com>
2025-01-07 23:59:43 +0000

    Create Pad.jsx

---
+++ soundPads/Pad.jsx
@@ -0,0 +1,9 @@
+export default function Pad(props) {
+    
+    return (
+        <button 
+            style={{backgroundColor: props.color}}
+            className={props.on ? "on" : undefined}
+        ></button>
+    )
+}
