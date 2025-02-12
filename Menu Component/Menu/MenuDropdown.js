a1b182f67101f0ad9b1021d4622169974daca8e0
snaqvi93 <abbas_naq@icloud.com>
2025-02-12 21:05:08 +0000

    Update MenuDropdown.js

--- Menu Component/Menu/MenuDropdown.js
+++ Menu Component/Menu/MenuDropdown.js
@@ -4,9 +4,14 @@ import { MenuContext } from "./Menu"
 export default function MenuDropdown({ children }) {
     const { open } = React.useContext(MenuContext)
 
-    return open ? (
-        <div className="menu-dropdown">
-            {children}
-        </div>
-    ) : null
+    return (
+        <>
+            {open ? (
+                <div className="menu-dropdown">
+                    {children}
+                </div>
+            ) : null
+            }
+        </>
+    )
 }
