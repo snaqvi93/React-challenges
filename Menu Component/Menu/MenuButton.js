2b6694916923773b865e3341ffd03c88c79b640e
snaqvi93 <abbas_naq@icloud.com>
2025-02-12 21:04:43 +0000

    Update MenuButton.js

--- Menu Component/Menu/MenuButton.js
+++ Menu Component/Menu/MenuButton.js
@@ -3,8 +3,8 @@ import Button from "../Button/Button"
 import { MenuContext } from "./Menu"
 
 export default function MenuButton({ children }) {
-    const { toggle } = React.useContext(MenuContext)
+    const { toggleOpen } = React.useContext(MenuContext)
     return (
-        <Button onClick={toggle}>{children}</Button>
+        <Button onClick={toggleOpen}>{children}</Button>
     )
 }
